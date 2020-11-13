1e70c419b1e51bf286848eb96d3b455065f34b4c
Bryson Palmer <brysonpalmer@gmail.com>
2020-11-12 23:04:53 -0800

    Finished creating the sort high scores function which returns highest to lowest scores.

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -3,8 +3,10 @@ var playAgain = document.querySelector( "#play-Again" );
 var clearAllEl = document.querySelector( "#clear-all" );
 var initListEl = document.querySelector( "#initials-List" );
 
-// Create array of high scores to hold objects of user info
-var input_Array = []; 
+
+var input_Array = []; // Create array of high scores to hold objects of user info
+var tempArray = [];   // Empty array to hold only the scores to be sorted
+var hsPointer = 0;        // Global high score pointer
 
 // Runs the high scores page
 function highScores( list ) {
@@ -21,11 +23,11 @@ function highScores( list ) {
     }
     console.log(input_Array)                            // Log check array of objects
 
-    for ( i = 0; i < input_Array.length; i++ ) {  
-        console.log("You made it in the for loop");
+    sortHighScores();                                   // Call functin sorting high scores list from highest to lowest
+    
+    for ( i = 0; i < input_Array.length; i++ ) { 
 
-        var li = document.createElement("li");          // Create new li element and store it in var
-        console.log(li);
+        var li = document.createElement("li");          // Create new <li> element and store it in var
         
         // Grab user initials and score in each object in array and assign to the text content of the li element
         li.textContent = input_Array[i].user_Initials + "..........." + input_Array[i].seconds_Left; 
@@ -35,6 +37,40 @@ function highScores( list ) {
     }
 }
 highScores();
+
+// Sorts high scores from highest to lowest
+function sortHighScores() {
+    
+    function compareObjects( object1, object2, key ) {
+        var obj1 = object1[key];
+        var obj2 = object2[key];
+
+        if (obj1 < obj2) {
+            return 1;
+        }
+        if (obj1 > obj2) {
+            return -1
+        }
+        return 0;
+    }
+
+    input_Array.sort((score1, score2) => {
+        return compareObjects(score1, score2, 'seconds_Left')
+    })
+    
+    
+    // for ( hsPointer = 0; hsPointer < input_Array.length; hsPointer++ ) {
+    //     tempArray.push( input_Array[hsPointer].seconds_Left );
+    // }
+    // tempArray.sort(function(a, b){return b-a });
+    
+
+    // for ( hsPointer = 0; hsPointer < input_Array.length; hsPointer++ ) {
+    //     input_Array[hsPointer].seconds_Left = tempArray[hsPointer];
+    // }
+    // console.log( tempArray );
+    // console.log( input_Array );
+}
 
 
 // Starting game play again
@@ -53,4 +89,4 @@ function clearAll() {
         localStorage.clear();                     // Clears local storage
     }
 }  
-clearAllEl.addEventListener("click", clearAll); // When user clicks the play again button run play again high scores
+clearAllEl.addEventListener("click", clearAll); // When user clicks the clear all button run clearAll high scores
