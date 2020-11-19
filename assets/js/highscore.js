9c163576b2cd3711732b6178cc33df7103133943
Bryson Palmer <brysonpalmer@gmail.com>
2020-11-18 17:49:51 -0800

    Adding final comments to the game files.

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,4 +1,4 @@
-
+// Declaring and pointing to elements in the index html
 var playAgain = document.querySelector( "#play-Again" );
 var clearAllEl = document.querySelector( "#clear-all" );
 var initListEl = document.querySelector( "#initials-List" );
@@ -25,37 +25,37 @@ function highScores( list ) {
 
     sortHighScores();                                   // Call functin sorting high scores list from highest to lowest
     
-    for ( i = 0; i < input_Array.length; i++ ) { 
+    for ( i = 0; i < input_Array.length; i++ ) {        // For loop moving through input array
 
         var li = document.createElement("li");          // Create new <li> element and store it in var
         
-        // Grab user initials and score in each object in array and assign to the text content of the li element
+        // Grab every object of user initials and score in array and assign to the text content of the li element
         li.textContent = input_Array[i].user_Initials + "..........." + input_Array[i].seconds_Left; 
         console.log(li.textContent);
  
         initListEl.appendChild(li);                     // Appending the content of li to parent element initListEl
     }
 }
-highScores();
+highScores();                                           // Call highscores function
 
-// Sorts high scores from highest to lowest
+// Sorts high scores from highest to lowest             
 function sortHighScores() {
     
-    function compareObjects( object1, object2, key ) {
-        var obj1 = object1[key];
-        var obj2 = object2[key];
+    function compareObjects( object1, object2, key ) { // Function for comparing objects at a specific key
+        var obj1 = object1[key];                       // New variable holding object 1 key value
+        var obj2 = object2[key];                       // New variable holding object 2 key value
 
-        if (obj1 < obj2) {
-            return 1;
+        if (obj1 < obj2) {                             // If obj1 is less than obj2
+            return 1;                                  // Return 1
         }
-        if (obj1 > obj2) {
-            return -1
+        if (obj1 > obj2) {                             // If obj1 is greater than obj2
+            return -1                                  // Return -1
         }
-        return 0;
+        return 0;                                      // Return 0 if both obj1 === obj2
     }
 
-    input_Array.sort((score1, score2) => {
-        return compareObjects(score1, score2, 'seconds_Left')
+    input_Array.sort((score1, score2) => {             // Sort method sorting input array and passing score1 and score2 as arguments
+        return compareObjects(score1, score2, 'seconds_Left') // Return compare objects method returns sorted score from High to low
     })
 }
 
